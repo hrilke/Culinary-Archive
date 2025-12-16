823e8995222790e071b77a3c22d2c3acd8b818e8
Hrishikesh Shelke <dev.hrishi@outlook.com>
2025-12-16 13:35:30 +0530

    spring project setup : github repo + spring initilizr

---
+++ src/main/java/com/cookbook/culinary_archive/CulinaryArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook.culinary_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookbook/culinary_archive/CulinaryArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookbook.culinary_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
